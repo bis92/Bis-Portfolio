d3161050587a5d396d5b7e3fc2dc87c2167f2e9c
InseobByeon <62586750+bis92@users.noreply.github.com>
2022-10-02 15:18:45 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -10,3 +10,15 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 })
+
+// Navigation When on Click Button
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    const targetMenu = document.querySelector(link);
+    targetMenu.scrollIntoView({ behavior: 'smooth'});
+})
